2212b87c4ffd285dba69841c284a8c97ce3afd45
Ivy <470888496@qq.com>
2017-09-22 21:50:23 +0800

    the index page extend from layout.main

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,17 +8,24 @@ class PostController extends Controller
 {
     // 列表
     public function index(){
-        return view("post/index");// view有两个参数 第一个参数是模板相对地址 第二个参数是数组，传给模板的变量有哪些
+        $posts = [
+            ['title' => 'this is title1'],
+            ['title' => 'this is title2'],
+            ['title' => 'this is title3']
+        ];
+        return view("post/index", compact('posts'));
+        // view有两个参数 第一个参数是模板相对地址 第二个参数是数组，传给模板的变量有哪些
+        // compact 创建一个包含变量名和它们的值的数组
 
     }
     // 详情页面
     public function show(){
-        return view("post/show");
+        return view("post/show",['title' => "this is title" , 'isShow' => false]);
 
     }
     // 创建页面
     public function create(){
-        return;
+        return view("post/create");
 
     }
     // 创建逻辑
@@ -28,7 +35,7 @@ class PostController extends Controller
     }
     // 编辑页面
     public function edit(){
-        return;
+        return view("post/edit");
 
     }
     // 编辑逻辑
--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -8,9 +8,6 @@
     <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
     <meta name="description" content="">
     <meta name="author" content="">
-    <meta name="csrf-token" content="4BfTBDF90Mjp8hdoie6QGDPJF2J5AgmpsC9ddFHD">
-
-
     <title>laravel for blog</title>
 
     <!-- Bootstrap core CSS -->
@@ -34,39 +31,37 @@
 
 <div class="blog-masthead">
     <div class="container">
-        <form action="/posts/search" method="GET">
-            <ul class="nav navbar-nav navbar-left">
-                <li>
-                    <a class="blog-nav-item " href="/posts">首页</a>
-                </li>
-                <li>
-                    <a class="blog-nav-item" href="/posts/create">写文章</a>
-                </li>
-                <li>
-                    <a class="blog-nav-item" href="/notices">通知</a>
-                </li>
-                <li>
-                    <input name="query" type="text" value="" class="form-control" style="margin-top:10px" placeholder="搜索词">
-                </li>
-                <li>
-                    <button class="btn btn-default" style="margin-top:10px" type="submit">Go!</button>
-                </li>
-            </ul>
+        <ul class="nav navbar-nav navbar-left">
+            <li>
+                <a class="blog-nav-item " href="/posts">首页</a>
+            </li>
+            <li>
+                <a class="blog-nav-item" href="/posts/create">写文章</a>
+            </li>
+            <li>
+                <a class="blog-nav-item" href="/notices">通知</a>
+            </li>
+            <li>
+                <input name="query" type="text" value="" class="form-control" style="margin-top:10px" placeholder="搜索词">
+            </li>
+            <li>
+                <button class="btn btn-default" style="margin-top:10px" type="submit">Go!</button>
+            </li>
+        </ul>
 
-            <ul class="nav navbar-nav navbar-right">
-                <li class="dropdown">
-                    <div>
-                        <img src="/storage/9f0b0809fd136c389c20f949baae3957/iBkvipBCiX6cHitZSdTaXydpen5PBiul7yYCc88O.jpeg" alt="" class="img-rounded" style="border-radius:500px; height: 30px">
-                        <a href="#" class="blog-nav-item dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Kassandra Ankunding2  <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="/user/5">我的主页</a></li>
-                            <li><a href="/user/5/setting">个人设置</a></li>
-                            <li><a href="/logout">登出</a></li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
-        </form>
+        <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+                <div>
+                    <img src="/storage/9f0b0809fd136c389c20f949baae3957/iBkvipBCiX6cHitZSdTaXydpen5PBiul7yYCc88O.jpeg" alt="" class="img-rounded" style="border-radius:500px; height: 30px">
+                    <a href="#" class="blog-nav-item dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Kassandra Ankunding2  <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="/user/5">我的主页</a></li>
+                        <li><a href="/user/5/setting">个人设置</a></li>
+                        <li><a href="/logout">登出</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
     </div>
 </div>
 <div class="container">
@@ -76,65 +71,22 @@
 
     <div class="row">
 
-
         <div class="col-sm-8 blog-main">
-            <div class="blog-post">
-                <div style="display:inline-flex">
-                    <h2 class="blog-post-title">{{$title}}</h2>
-                    @if($isShow)
-                    <a style="margin: auto"  href="/posts/62/edit">
-                        <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-                    </a>
-                    @endif
-                    <a style="margin: auto"  href="/posts/62/delete">
-                        <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-                    </a>
+            <form action="/posts" method="POST">
+                <input type="hidden" name="_token" value="MESUY3topeHgvFqsy9EcM916UWQq6khiGHM91wHy">
+                <div class="form-group">
+                    <label>标题</label>
+                    <input name="title" type="text" class="form-control" placeholder="这里是标题">
                 </div>
-
-                <p class="blog-post-meta">May 14, 2017 by <a href="#">Kassandra Ankunding2</a></p>
-
-                <p><p>你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好<img src="http://127.0.0.1:8000/storage/72c76b674ec8793fcfd6555ff371bfbd/nxC9ozLfkORmoY92q9lPsejXchVvdNO2cwHiR2Jf.jpeg" alt="63" style="max-width: 100%;">你好你好似懂非懂说</p><p><br></p></p>
-                <div>
-                    <a href="/posts/62/zan" type="button" class="btn btn-primary btn-lg">赞</a>
-
+                <div class="form-group">
+                    <label>内容</label>
+                    <textarea id="content"  style="height:400px;max-height:500px;" name="content" class="form-control" placeholder="这里是内容"></textarea>
                 </div>
-            </div>
-
-            <div class="panel panel-default">
-                <!-- Default panel contents -->
-                <div class="panel-heading">评论</div>
-
-                <!-- List group -->
-                <ul class="list-group">
-                    <li class="list-group-item">
-                        <h5>2017-05-28 10:15:08 by Kassandra Ankunding2</h5>
-                        <div>
-                            这是第一个评论这是第一个评论这是第一个评论这是第一个评论这是第一个评论这是第一个评论这是第一个评论这是第一个评论这是第一个评论
-                        </div>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="panel panel-default">
-                <!-- Default panel contents -->
-                <div class="panel-heading">发表评论</div>
-
-                <!-- List group -->
-                <ul class="list-group">
-                    <form action="/posts/comment" method="post">
-                        <input type="hidden" name="_token" value="4BfTBDF90Mjp8hdoie6QGDPJF2J5AgmpsC9ddFHD">
-                        <input type="hidden" name="post_id" value="62"/>
-                        <li class="list-group-item">
-                            <textarea name="content" class="form-control" rows="10"></textarea>
-                            <button class="btn btn-default" type="submit">提交</button>
-                        </li>
-                    </form>
-
-                </ul>
-            </div>
+                <button type="submit" class="btn btn-default">提交</button>
+            </form>
+            <br>
 
         </div><!-- /.blog-main -->
-
 
 
 
