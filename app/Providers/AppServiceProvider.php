3c52d3c770025e49bf1837e42e51278c577d4c90
Ivy <470888496@qq.com>
2017-09-24 01:04:31 +0800

    migrate table

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+
 use Illuminate\Support\ServiceProvider;
+//use Illuminate\Support\Facades\Schema;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //boot 函数启动之后执行
+//        laravel string 默认字符编码 mb4string, 4个bytes 对应一个字符 so 767/4 = 191.75
+//        Schema::defaultStringLenth(191); //设置laravel默认的string长度
     }
 
     /**
@@ -23,6 +28,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        //register 函数启动之前执行
     }
 }
+// fix SQL 1071 specified key was too long ; max key length is 767 bytes
