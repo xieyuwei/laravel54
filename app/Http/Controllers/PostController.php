85e73b7a2092aaa549c578b81536ca85f80d8c1c
Ivy <470888496@qq.com>
2017-09-25 00:19:59 +0800

    tinker modify&populate data and paginate

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,16 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
     // 列表
     public function index(){
-        $posts = [
-            ['title' => 'this is title1'],
-            ['title' => 'this is title2'],
-            ['title' => 'this is title3']
-        ];
+        $posts = Post::orderBy('created_at','desc')->paginate(6);
         return view("post/index", compact('posts'));
         // view有两个参数 第一个参数是模板相对地址 第二个参数是数组，传给模板的变量有哪些
         // compact 创建一个包含变量名和它们的值的数组
--- config/app.php
+++ config/app.php
@@ -63,8 +63,8 @@ return [
     | ahead and set this to a sensible default for you out of the box.
     |
     */
-
-    'timezone' => 'UTC',
+//设置时区
+    'timezone' => 'Asia/Shanghai',
 
     /*
     |--------------------------------------------------------------------------
