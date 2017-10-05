0f29d4cd5ff99458d28373803e524e55db2a2c89
Ivy <470888496@qq.com>
2017-10-06 00:15:59 +0800

    register page

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,15 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class RegisterController extends Controller
 {
     public function index(){
-        //TODO: 和PostController里的view写的不一样
+        //TODO: 和PostController里的view写的不一样(两种写法都work)
         return view('register.index');
     }
 
     public function register(){
-        return ;
+        //实际是一个表单提交，往user表里添加信息
+        //验证
+        $this->validate(request(),[
+            'name' =>'required|min:2|max:40|unique:users,name',
+            'email' =>'required|unique:users,email|email',
+            'password' => 'required|min:6|max:15|comfirmed'
+        ]);
+        //逻辑
+        $name = request('name');
+        $email = request('email');
+        $password = bcrypt(request('password')); //明文加密成密文
+        $user = User::create(compact('name','email','password');
+        //渲染
+        return redirect('/login');
     }
 }
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -34,7 +34,7 @@
 <div class="container">
 
     <form class="form-signin" method="POST" action="/register">
-        <input type="hidden" name="_token" value="MESUY3topeHgvFqsy9EcM916UWQq6khiGHM91wHy">
+        {{csrf_field()}}
         <h2 class="form-signin-heading">请注册</h2>
         <label for="name" class="sr-only">名字</label>
         <input type="text" name="name" id="name" class="form-control" placeholder="名字" required autofocus>
