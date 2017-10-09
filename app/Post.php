f39cd30ba86b5a13ad9bfc3c815063e368b6f6a0
Ivy <470888496@qq.com>
2017-10-09 15:54:42 +0800

    Comment model and Post model associate

--- app/Post.php
+++ app/Post.php
@@ -6,18 +6,22 @@ use App\Model;
 
 
 class Post extends Model
-{
-//    protected $guarded = [];//不可以注入的字段
-//    protected $fillable = ['title','content'];//可以注入数据字段
-//
-//    如果什么都不写 ，对应的表就是posts
-//    如果不对应posts,就要指定他的table属性
-//    protected $table = "post2";
+    {
+    //    protected $guarded = [];//不可以注入的字段
+    //    protected $fillable = ['title','content'];//可以注入数据字段
 
-//    关联用户Eloquent ORM模型关联
-//    hasOne外键保存在关联表中   belongsTo外键放置在主表中
+    //    如果什么都不写 ，对应的表就是posts
+    //    如果不对应posts,就要指定他的table属性
+    //    protected $table = "post2";
+
+    //    关联用户Eloquent ORM模型关联
+    //    TODO: 下面的评论模型完成了一对多和一对多反向，为什么这个不需要相互关联呢？
     public function user(){
         return $this->belongsTo('App\User');
       }
-
+    //    评论模型  绑定后可以使用Post 来获取 Comment了
+    public function comments(){
+        //asc是指定列按升序排列，desc则是指定列按降序排列
+        return $this->hasMany('App\Comment')->orderBy('created_at','desc');
+    }
 }
