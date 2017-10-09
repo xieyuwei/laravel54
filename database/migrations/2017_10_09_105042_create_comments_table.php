78be78426903c274418eee334bc1ce0354917e43
Ivy <470888496@qq.com>
2017-10-09 11:45:59 +0800

    create comments table

---
+++ database/migrations/2017_10_09_105042_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');//自增ID
+            $table->integer('post_id')->default("0");//VARCHAR
+            $table->text('content');
+            $table->integer('user_id')->default("0");//INT
+            $table->timestamps();//TIMESTAMP  (created_at updated_at)
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
